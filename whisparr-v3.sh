9dc31c3e0cbe4393e28ef687d24420d867407053
Athena <210293345+athena-sh@users.noreply.github.com>
2025-12-29 00:12:55 -0500

    Refactor Whisparr v3 installation and service setup
    Updated the installation script for Whisparr v3 to download from a new repository and added service creation steps.

--- whisparr-v3.sh
+++ whisparr-v3.sh
@@ -1,63 +1,66 @@
 #!/usr/bin/env bash
 
-# Download and patch build.func to use custom install script repo
-BUILD_FUNC=$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
-BUILD_FUNC_PATCHED=$(echo "$BUILD_FUNC" | sed 's|https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/install/${var_install}.sh|https://raw.githubusercontent.com/athena-sh/whisparr-v3-builds/main/whisparr-v3-install.sh|g')
-source <(echo "$BUILD_FUNC_PATCHED")
-
 # Copyright (c) 2021-2025 tteck
 # Author: tteck (tteckster)
 # Co-Author: athena-sh
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/Whisparr/Whisparr (eros branch)
 
-APP="Whisparr-v3"
-var_tags="${var_tags:-arr;whisparr;eros-branch;community-script}"
-var_cpu="${var_cpu:-2}"
-var_ram="${var_ram:-1024}"
-var_disk="${var_disk:-12}"
-var_os="${var_os:-debian}"
-var_version="${var_version:-13}"
-var_unprivileged="${var_unprivileged:-1}"
-
-header_info "$APP"
-variables
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
 color
+verb_ip6
 catch_errors
+setting_up_container
+network_check
+update_os
 
-function update_script() {
-  header_info
-  check_container_storage
-  check_container_resources
-  if [[ ! -d /var/lib/whisparr ]]; then
-    msg_error "No ${APP} Installation Found!"
-    exit
-  fi
-  msg_info "Updating ${APP}"
-  systemctl stop whisparr
-  RELEASE=$(curl -s https://api.github.com/repos/athena-sh/whisparr-v3-builds/releases | grep -m 1 "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-  if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
-    msg_info "Updating ${APP} to ${RELEASE}"
-    cd /opt
-    rm -rf Whisparr
-    wget -q https://github.com/athena-sh/whisparr-v3-builds/releases/download/${RELEASE}/Whisparr-v3-eros.tar.gz
-    tar -xzf Whisparr-v3-eros.tar.gz
-    echo "${RELEASE}" > /opt/${APP}_version.txt
-    rm Whisparr-v3-eros.tar.gz
-    msg_ok "Updated ${APP} to ${RELEASE}"
-  else
-    msg_ok "No update required. ${APP} is already at ${RELEASE}"
-  fi
-  systemctl start whisparr
-  msg_ok "Updated Successfully"
-  exit
-}
+msg_info "Installing Dependencies"
+$STD apt install -y sqlite3
+msg_ok "Installed Dependencies"
 
-start
-build_container
-description
+msg_info "Installing Whisparr v3 (Eros)"
+mkdir -p /var/lib/whisparr/
+chmod 775 /var/lib/whisparr/
 
-msg_ok "Completed Successfully!\n"
-echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
-echo -e "${INFO}${YW} Access it using the following URL:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:6969${CL}
+# Download pre-compiled Whisparr v3 from GitHub releases
+cd /opt || exit
+RELEASE=$(curl -s https://api.github.com/repos/athena-sh/whisparr-v3-builds/releases | grep -m 1 "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+msg_info "Downloading Whisparr v3 ${RELEASE}"
+$STD curl -fsSL "https://github.com/athena-sh/whisparr-v3-builds/releases/download/${RELEASE}/Whisparr-v3-eros.tar.gz" -o Whisparr-v3-eros.tar.gz
+$STD tar -xzf Whisparr-v3-eros.tar.gz
+chmod 775 /opt/Whisparr
+rm -f Whisparr-v3-eros.tar.gz
+echo "${RELEASE}" > /opt/Whisparr-v3_version.txt
+msg_ok "Installed Whisparr v3 (Eros) ${RELEASE}"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/whisparr.service
+[Unit]
+Description=Whisparr v3 (Eros) Daemon
+After=network.target
+
+[Service]
+User=whisparr
+Group=whisparr
+Type=simple
+ExecStart=/opt/Whisparr/Whisparr -nobrowser -data=/var/lib/whisparr
+TimeoutStopSec=20
+KillMode=process
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+msg_info "Creating Service User"
+useradd --system --no-create-home --shell /usr/sbin/nologin whisparr
+chown -R whisparr:whisparr /var/lib/whisparr
+chown -R whisparr:whisparr /opt/Whisparr
+msg_ok "Created Service User"
+
+systemctl enable --now -q whisparr
+msg_ok "Created Service"
+
+motd_ssh
+customize
+cleanup_lxc
