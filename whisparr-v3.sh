6bd73f521bf8bd240f7524ab55a99102552ff796
Athena <210293345+athena-sh@users.noreply.github.com>
2025-12-28 22:49:40 -0500

    Update co-author in whisparr-v3.sh

--- whisparr-v3.sh
+++ whisparr-v3.sh
@@ -2,7 +2,7 @@
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2025 tteck
 # Author: tteck (tteckster)
-# Co-Author: [Your GitHub Username]
+# Co-Author: [athena-sh]
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/Whisparr/Whisparr (eros branch)
 
